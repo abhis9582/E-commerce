e67ecb74c7b3aa0ddccdd907fc97d49c9e9e0ba8
Abhishek Kumar <abhishek89601679@gmail.com>
2024-06-28 22:05:41 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import Navigation from './customer/components/Navigation/Navigation';
 import HomePage from './customer/pages/HomePage/HomePage';
 import Product from './customer/components/Product/Product';
+import ProductDetails from './customer/components/ProductDetails/ProductDetails';
+import Cart from './customer/components/Cart/Cart';
+import Checkout from "./customer/components/Checkout/Checkout";
 function App() {
   return (
     <div className="">
       <Navigation />
       <div>
         {/* <HomePage /> */}
-        <Product />
+        {/* <Product /> */}
+        {/* <ProductDetails/> */}
+        {/* <Cart /> */}
+        <Checkout />
       </div>
       <Footer />
     </div>
